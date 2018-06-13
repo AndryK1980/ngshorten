04aa2572457a9d813d50c8be80a5f98f9da5a187
AndryK <a.kriganovsky@gmail.com>
2018-06-13 19:38:35 +0300

    hiroku

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 //Install express server
 const express = require('express');
+const http= require('http');
 const path = require('path');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(path.resolve(__dirname + '/dist')));
 
 app.get('/*', function(req,res) {
 
-res.sendfile(path.join(__dirname+'/dist/index.html'));
+res.sendFile(path.resolve(__dirname+'/dist/ngshort/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+const port=process.env.PORT || '8080';
+app.set('port',port);
+const server=http.createServer(app);
+server.listen(port, ()=>console.log('Be happy'));
 
